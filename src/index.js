fc31d27eb99ba33e3042b9dd4235eb7f53fb499c
Silvanos Eric <ericoderito@gmail.com>
2024-07-11 19:08:13 +0300

    feat: enhance UI and add task manipulation features
    - Update DOCTYPE and improve self-closing tags in index.html for HTML5 compliance.
    - Refactor form submission to include required attribute and change submit input to button with
      style.
    - Enhance task list with editable task descriptions and delete functionality in index.js.
    - Implement global CSS reset and custom styles for form, buttons, and task list in style.css.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // get a reference to all elements of interest
+  const inputEl = document.querySelector("input#new-task-description");
+  const createTaskFormEl = document.querySelector("form#create-task-form");
+  const taskListEl = document.querySelector("ul#tasks");
+
+  // Extract the value from the form input field,
+  // and add it to the list
+  createTaskFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputElValue = inputEl.value;
+
+    const listItemEl = document.createElement("li");
+
+    const spanEl = document.createElement("span");
+    spanEl.textContent = inputElValue;
+    spanEl.contentEditable = true;
+    spanEl.title = "Click to edit!";
+
+    listItemEl.appendChild(spanEl);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.classList.add("btn", "btn-danger");
+
+    listItemEl.appendChild(deleteButton);
+
+    taskListEl.appendChild(listItemEl);
+
+    inputEl.value = "";
+  });
+
+  // Functionality to remove an item from the task list
+  taskListEl.addEventListener("click", (event) => {
+    if (event.target.matches("button")) {
+      const listItemEl = event.target.parentElement;
+      listItemEl.remove();
+    }
+  });
+
+  // Ability to edit tasks
+  taskListEl.addEventListener("click", (event) => {
+    if (event.target.matches("li")) {
+      event.target.contenteditable = true;
+    }
+  });
 });
